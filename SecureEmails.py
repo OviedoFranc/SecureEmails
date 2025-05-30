ccabd9b10f9f1bda51bb4be64794e44bd84657e4
OviedoFranc <oviedofranc@gmail.com>
2025-05-30 13:00:25 -0300

    bonus complete

--- SecureEmails.py
+++ SecureEmails.py
@@ -1,5 +1,7 @@
 import os.path # permite obtener el path, os es del sistema operativo.
 import base64
+import re
+import mimetypes # link documentacion https://mimetype.io/all-types
 from datetime import datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,6 +14,9 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 PUBLIC_KEY = "1017525713366-jkriu4gsfepugq0livovv6ntfsiqndop.apps.googleusercontent.com"
 CLIENT_SECRET = "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"
+WHITELIST_ENTERPRISE = ["google, empresa"]
+CRITICAL_WORDS = ["contraseña","confidencial"]
+CRITICAL_EXTENSIONS = [".exe",".zip",".js",".bat"]
 
 def get_creds():
   client_config = {
@@ -51,23 +56,43 @@ def decode_body_message(payload):
     
   return "body can not be redeable"
 
+#funcion encargada de buscar las claves criticas
 def search_words(text):
-  critical_words = ["contraseña","confidencial"]
   word = text.split()
   for w in word:
-    if w in critical_words:
+    if w in CRITICAL_WORDS:
       return w
   return None
 
 def get_message_critical_word(header, sender, critical_word):
-  return ("\nCritical word "+ critical_word + " from " + sender + " on email with subject " + header  + "  - Date registered at "+ datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+  return ("\nCritical word "+ critical_word + " from " + sender + " on email with subject " + header)
 
 def file_handler_alert(warning_message):
-  #mas manejo del file https://www.w3schools.com/python/python_file_open.asp
+  #mas manejo de file https://www.w3schools.com/python/python_file_open.asp
   #lo abrimos con "a" para posicionarnos al final del archivo, ya que al escribir sobrescribe encima si estoy al incio.
   with open("alertas.txt", "a") as f:
-    f.write(warning_message)
+    f.write(warning_message+" - Date registered at " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     f.close()
+
+def email_secure(email):
+  # busca que inicie (^) con cualquier caracter (. y * para las cantidad que quiera) hasta @ + mail que este en la lista blanca y que finalice exactamente con .com (\ es para que interprete exactamente el . y no lo tome como cualquier caracter)
+  #unicamente acepta empresas en la lista blanca, esto sucede debido a que Empresa no es igual a EMPRESA, quiza alguien se suplantacion de identidad con dicho metodo.
+  mail_accepted = re.search("^.*@\*({WHITELIST_ENTERPRISE})\.com$",email)
+  if(mail_accepted): return true
+  return false
+
+#funcion encarga de revisar los adjuntos en los emails
+def check_attach(payload, header,sender):
+  #tenemos todos los tipos de mime type en este link https://mimetype.io/all-types por lo cual vemos que podemos obtener su file type
+  for part in payload["parts"]:
+    #si no lo encuentro sigo buscando, sino paso de largo todo el proceso
+    if not "filename" in part and part["filename"]: continue
+    mime_type = part["mimeType"]
+    file_type = mimetypes.guess_extension(mime_type) or "unknown"
+    #si encuentro que esta en la extensiones, mando un mensaje al handler de lo que encontre!
+    if file_type.lower() in CRITICAL_EXTENSIONS:
+      file_handler_alert("File with extension "+file_type+" found on email from "+sender+" with subject"+header)
+
 
 def main():
   creds = get_creds()
@@ -101,20 +126,23 @@ def main():
       #        filename?: str  # Para adjuntos (ej: "doc.pdf")
 
       message = service.users().messages().get(userId="me", id=mail["id"]).execute()
-      # next obtiene el siguiente valor que cumpla condicion | generator expression devuelve una lista part de value
-      header = next(part["value"] for part in message["payload"]["headers"] if part["name"] == "Subject")
-      body_message = decode_body_message(message["payload"])
+      #obtengo primero quien me lo envio
+      sender = next(part["value"] for part in message["payload"]["headers"] if part["name"] == "From")
+      #si el correo se encuentra dentro de mi lista blanca no considero verificar su contenido, ya que es seguro
+      if( not email_secure(sender) ):
+        # next obtiene el siguiente valor que cumpla condicion | generator expression devuelve una lista part de value
+        header = next(part["value"] for part in message["payload"]["headers"] if part["name"] == "Subject")
+        body_message = decode_body_message(message["payload"])
 
-      #busco la palabra clave y la intento guardar, en cuyo caso sera None, sino disparo el handler
-      critical_word = search_words(header) or search_words(body_message)
-      if critical_word:
-        #lo coloco acá para no hacer proceso extra en caso de no ser necesario
-        sender = next(part["value"] for part in message["payload"]["headers"] if part["name"] == "From")
-        warning_message = get_message_critical_word(header, sender, critical_word)
-        file_handler_alert(warning_message)
-        print(warning_message) 
+        #busco la palabra clave y la intento guardar, si existe disparo el handler para manejar la filtracion de la palabra,sino tendré un None.
+        critical_word = search_words(header) or search_words(body_message)
+        if critical_word:
+          warning_message = get_message_critical_word(header, sender, critical_word)
+          file_handler_alert(warning_message)
+          print(warning_message) 
 
-      if message["payload"]["filename"]: print("Adjunto:", message["payload"]["filename"][0])
+        #parte de comprobacion de archivo adjunto
+        check_attach(message["payload"], header,sender)
 
   except HttpError as error:
     print(f"An error occurred: {error}")
